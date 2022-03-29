dd398d81760f9cbaf4647ecdc660a52dab0b8f41
Sergio Valadez <sergioval@gmail.com>
2022-03-29 13:10:16 -0600

    Add function createTags

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ createTags(e.target.value)
 });
 
 function createTags(input) {
-const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-console.log(tags)
-    tagsEl.innerHTML = '';
+const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+console.log(tags);
+tagsEl.innerHTML = '';
+tags.forEach(tag => {
+const tagEl = document.createElement('span');
+tagEl.classList.add('tag');
+tagEl.innerText = tag;
+tagsEl.appendChild(tagEl);
+})
+
 }
