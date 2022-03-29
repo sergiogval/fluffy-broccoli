7b18d9052bbcb187db260acecb231d7c227c5d16
Sergio Valadez <sergioval@gmail.com>
2022-03-29 13:28:37 -0600

    Add js functionallity

--- script.js
+++ script.js
@@ -5,18 +5,48 @@ const textarea = document.getElementById('textarea');
 textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
-createTags(e.target.value)
+    createTags(e.target.value);
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+        }, 10)
+        e.target.value = '';
+        randomSelect();
+    }
+
 });
 
 function createTags(input) {
-const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
-console.log(tags);
-tagsEl.innerHTML = '';
-tags.forEach(tag => {
-const tagEl = document.createElement('span');
-tagEl.classList.add('tag');
-tagEl.innerText = tag;
-tagsEl.appendChild(tagEl);
-})
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+    console.log(tags);
+    tagsEl.innerHTML = '';
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl);
+    })
+}
 
+function randomSelect() {
+    const times = 30;
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        highlightTag(randomTag)
+        setTimeout(() => {
+            unhighlightTag(randomTag)
+        }, 100);
+    }, 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
